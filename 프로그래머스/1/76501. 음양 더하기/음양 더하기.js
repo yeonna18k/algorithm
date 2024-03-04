30264638680d23875329484ebffda643d62c618d
yeonna18k <134368869+yeonna18k@users.noreply.github.com>
2024-03-04 09:34:46 +0900

    [level 1] Title: 음양 더하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,4 +1,4 @@
 function solution(absolutes, signs) {
-    let arr = absolutes.map((e, i) => signs[i] ? e : e * -1);
+    let arr = absolutes.map((e, i) => signs[i] ? e : -e);
     return arr.reduce((p, c) => p + c);
 }
