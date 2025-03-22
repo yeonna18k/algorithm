3b70e70084663c421478598db1042f92379a1938
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-22 17:10:55 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,4 @@
+function solution(price) {
+    const discount = price >= 500000 ? price * 0.8 : (price >= 300000 ? price * 0.9 : (price >= 100000 ? price * 0.95 : price));
+    return Math.floor(discount)
+}
