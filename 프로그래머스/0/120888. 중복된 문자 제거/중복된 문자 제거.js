d8642fc2125d2965c44f4c4aa75840da128e5404
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-24 23:37:58 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -3,5 +3,6 @@ function solution(my_string) {
     for (let i=0; i < my_string.length; i++) {
         if (!arr.includes(my_string[i])) arr.push(my_string[i])
     }
+    console.log([...new Set(my_string)].join(""))
     return arr.join("");
 }
