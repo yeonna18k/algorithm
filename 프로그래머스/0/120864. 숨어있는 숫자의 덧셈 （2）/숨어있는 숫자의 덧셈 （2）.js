4073620138ceabeff15198f8d5654aaf7785da85
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-25 23:52:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.replace(/[a-zA-Z]/g, " ").split(" ").filter(n => n !== "").reduce((cur, acc) => cur + parseInt(acc), 0);
+    return my_string.replace(/[a-zA-Z]/g, " ").split(" ").map(v => v * 1).reduce((cur, acc) => cur + acc, 0);
 }
