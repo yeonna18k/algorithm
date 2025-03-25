b8cd28fb68e800f5f64e33dd3e5f053ddbabbbe7
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-25 19:34:37 +0900

    [level 0] Title: k의 개수, Time: 18.62 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,9 +1,17 @@
+// function solution(i, j, k) {
+//     let answer = 0;
+//     for (let h = i; h <= j; h++) {
+//         let num_arr = h.toString().split("")
+//         let count = num_arr.filter(n => parseInt(n) === k).length
+//         answer += count
+//     }
+//     return answer;
+// }
+
 function solution(i, j, k) {
-    let answer = 0;
-    for (let h = i; h <= j; h++) {
-        let num_arr = h.toString().split("")
-        let count = num_arr.filter(n => parseInt(n) === k).length
-        answer += count
+    let string = ''
+    for (i; i <= j; i++) {
+        string += i
     }
-    return answer;
+    return string.split(k).length - 1;
 }
