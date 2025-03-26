c9a52f981d54694438c48b3dd0dffa18f4ef3a99
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-26 17:03:16 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,8 +1,14 @@
+// function solution(s) {
+//     let sort = [...s].sort();
+//     let news = ""
+//     for (let i = 0; i < sort.length; i++) {
+//         if (sort.filter(e => e === sort[i]).length === 1) news += sort[i]
+//     }
+//     return news;
+// }
+
 function solution(s) {
-    let sort = [...s].sort();
-    let news = ""
-    for (let i = 0; i < sort.length; i++) {
-        if (sort.filter(e => e === sort[i]).length === 1) news += sort[i]
-    }
-    return news;
+    let news = []
+    for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) news.push(c)
+    return news.sort().join("");
 }
