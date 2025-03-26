26a33460dfd2ec44e89f65298ad3933eadcdd21b
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-26 16:42:24 +0900

    [level 0] Title: 가까운 수, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,5 +1,10 @@
+// function solution(array, n) {
+//     array.sort((a, b) => a - b);
+//     let abs = array.map(a => Math.abs(a - n));
+//     return array[abs.indexOf(Math.min(...abs))];
+// }
+
 function solution(array, n) {
-    array.sort((a, b) => a - b);
-    let abs = array.map(a => Math.abs(a - n));
-    return array[abs.indexOf(Math.min(...abs))];
+    array.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || a - b)
+    return array[0]
 }
