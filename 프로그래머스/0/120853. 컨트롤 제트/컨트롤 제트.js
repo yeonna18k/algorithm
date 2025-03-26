2722ef9656f51337b4a2d2e9afdb6326cf8cfe17
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-26 18:02:38 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,13 +1,22 @@
-function solution(s) {
-    let arr = s.split(' ');
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "Z") {
-            sum += parseInt(arr[i - 1]) * -1
-        } else {
-            sum += parseInt(arr[i])
-        }
-    }
+// function solution(s) {
+//     let arr = s.split(' ');
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === "Z") {
+//             sum += parseInt(arr[i - 1]) * -1
+//         } else {
+//             sum += parseInt(arr[i])
+//         }
+//     }
     
-    return sum;
+//     return sum;
+// }
+
+function solution(s) {
+    let arr = []
+    s.split(" ").forEach(number => {
+        if (number === "Z") arr.pop()
+        else arr.push(number)
+    })
+    return arr.reduce((cur, acc) => cur + parseInt(acc), 0)
 }
