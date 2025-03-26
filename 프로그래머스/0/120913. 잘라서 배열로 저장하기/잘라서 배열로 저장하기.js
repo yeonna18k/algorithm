6c52fba6aed418070531fcc17931b78929c3b326
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-03-26 18:12:50 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -3,8 +3,6 @@ function solution(my_str, n) {
     let arr_str = [...my_str]
     while (arr_str.length > 0) {
         answer.push(arr_str.splice(0, n).join(""));
-        console.log(arr_str)
     }
-    
     return answer;
 }
