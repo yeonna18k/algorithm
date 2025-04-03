49a0afee8c05dc7824c1df4f50b4ca336a25db55
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-04-03 15:27:01 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,8 +1,9 @@
 function solution(my_string) {
-    let num = 0;
-    for (e in my_string.split("")) {
-
-
+    let arr = my_string.split(" ")
+    let sum = parseInt(arr[0]);
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] === "+") sum += parseInt(arr[i + 1])
+        else if (arr[i] === "-") sum -= parseInt(arr[i + 1])
     }
-    return 3+ 5;
+    return sum;
 }
