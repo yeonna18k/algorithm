2158a904f31a9a0799dc67bfaf5938032b05ec38
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-04-08 16:10:27 +0900

    [level 0] Title: 등수 매기기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,9 +1,5 @@
 function solution(score) {
     let score_arr = score.map(e => (e[0] + e[1]) / 2);
     let sorted = score.map(e => (e[0] + e[1]) / 2).sort((a, b) => b - a);
-    let index_arr = [];
-    for (let s of score_arr) {
-        index_arr.push(sorted.indexOf(s) + 1);
-    }
-    return index_arr;
+    return score_arr.map(e => sorted.indexOf(e) + 1);
 }
