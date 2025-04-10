f172503876d8c92cf854f2079d827fe499e21e17
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-04-10 19:10:59 +0900

    [level 0] Title: 특이한 정렬, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,10 @@
+function solution(numlist, n) {
+    return numlist.sort((a, b) => {
+             if (Math.abs(n - a) === Math.abs(n - b)) {
+        return b - a;
+    } else {
+        return Math.abs(n - a) - Math.abs(n - b);
+    };
+    }
+                       );
+}
