f4c09876712454eea429ae0b9176b66e4829b756
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-04-11 17:11:49 +0900

    [level 0] Title: 다항식 더하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,15 +1,29 @@
+// function solution(polynomial) {
+//     let co = 0;
+//     let cons = 0;
+//     for (let p of polynomial.split(" + ")) {
+//         p.includes("x") ? co += (parseInt(p.replace("x", "")) || 1) : cons += parseInt(p)
+//     }
+//     if (co > 0) {
+//         if (cons > 0) {
+//             return co === 1 ? "x" + " + " + cons : co + "x" + " + " + cons;
+//         } else {
+//             return co === 1 ? "x" : co + "x";
+//         }
+//     }
+//     return cons.toString();
+// }
+
 function solution(polynomial) {
-    let co = 0;
-    let cons = 0;
-    for (let p of polynomial.split(" + ")) {
-        p.includes("x") ? co += (parseInt(p.replace("x", "")) || 1) : cons += parseInt(p)
-    }
-    if (co > 0) {
-        if (cons > 0) {
-            return co === 1 ? "x" + " + " + cons : co + "x" + " + " + cons;
-        } else {
-            return co === 1 ? "x" : co + "x";
-        }
-    }
-    return cons.toString();
+    let polyarr = polynomial.split(" + ");
+    
+    let xNum = polyarr.filter(e => e.includes("x")).map(x => x.replace("x", "") || 1).reduce((cur, acc) => cur + parseInt(acc), 0);
+    let consNum = polyarr.filter(e => !e.includes("x")).reduce((cur, acc) => cur + parseInt(acc), 0);
+    
+    let answer = [];
+    
+    xNum > 0 && answer.push(xNum > 1 ? xNum + "x" : "x")
+    consNum > 0 && answer.push(consNum)
+    
+    return answer.join(" + ");
 }
