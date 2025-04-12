15c92b68e9d3dafa7a823f682e9988f3b7d87185
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-04-12 20:17:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,28 @@
+// function solution(quiz) {
+//     return quiz.map(el => { 
+//         return el.split(" = ").map(n => {
+//             let num = n.split(" ");
+//              if (num.length > 1) {
+//                  if (n.includes("-")) return parseInt(num[0]) - parseInt(num[2]);
+//                  if (n.includes("+")) return parseInt(num[0]) + parseInt(num[2])
+//              }
+            
+//         return Number(n)
+//         })
+        
+//     }).map(an => an[0] === an[1] ? "O" : "X");
+// }
+
+function solution(quiz) {
+    return quiz.map(el => {
+        const [equation, result] = el.split(" = ");
+        const [n1, operator, n2] = equation.split(" ");
+        let calcResult = true;
+        if (operator === "+") {
+            calcResult = Number(n1) + Number(n2) === Number(result)
+        } else {
+            calcResult = Number(n1) - Number(n2) === Number(result)
+        }
+        return calcResult ? "O" : "X"
+    })
+}
