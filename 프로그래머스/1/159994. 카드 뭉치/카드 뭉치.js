53ba03390a06faf4fe4f5cdf8ca963e6a1e3efa0
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-05-26 22:32:03 +0900

    [level 1] Title: 카드 뭉치, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,17 @@
+function solution(cards1, cards2, goal) {
+    let idx1 = 0;
+    let idx2 = 0;
+    
+    for (let i = 0; i < goal.length; i++) {
+        if (cards1[idx1] === goal[i]) {
+            idx1++;
+        } else if (cards2[idx2] === goal[i]) {
+            idx2++;
+        }
+        
+        else {
+            return "No";
+        }
+    }
+    return "Yes";
+}
