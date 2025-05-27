b6706d6f81aaf2a7c30694785831e5cd09944e68
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-05-27 17:06:33 +0900

    [level 1] Title: 폰켓몬, Time: 0.57 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
+    const max = nums.length / 2
     const set = new Set(nums);
     
-    if (set.size >= (nums.length / 2)) return (nums.length / 2);
-    else return set.size;
+    return set.size >= max ? max : set.size;
 }
