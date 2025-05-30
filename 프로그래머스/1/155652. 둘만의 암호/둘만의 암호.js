e99d85a81ba7628be6a467a53cc64efd3fe436e1
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-05-30 17:45:56 +0900

    [level 1] Title: 둘만의 암호, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -3,23 +3,13 @@ function solution(s, skip, index) {
     for (let i = 0; i < alphabet.length; i++) {
         alphabet[i] = String.fromCharCode(i + 65).toLowerCase()
     }
+    alphabet = alphabet.filter(e => !skip.includes(e))
 
     const sArr = s.split("")
-    const skipArr = skip.split("").map(e => alphabet.indexOf(e))
     
-    return sArr.map(al => {
-        let alphabetIndex = alphabet.indexOf(al);
-        
-        for (let i = 0; i < index; i++) {
-            alphabetIndex++;
-            if (alphabetIndex >= 26) alphabetIndex = 0;
-            
-            while (skipArr.indexOf(alphabetIndex) > -1) {
-                alphabetIndex++;
-                if (alphabetIndex >= 26) alphabetIndex = 0;
-            }
-        }
-
-        return alphabet[alphabetIndex]
+    return sArr.map(al =>  {
+        let newIdx = alphabet.indexOf(al) + index;
+        while (newIdx >= alphabet.length) newIdx = newIdx - alphabet.length;
+        return alphabet[newIdx]
     }).join("")
 }
