ee9d85352e89d47b0fe08687e2f85fbf11e53f50
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-06-02 17:58:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.82 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,22 @@
 function solution(participant, completion) {
-    participant.sort()
-    completion.sort()
+    // participant.sort()
+    // completion.sort()
+    // for (let i = 0; i < participant.length; i++) {
+    //     if (participant[i] !== completion[i]) {
+    //         return participant[i];
+    //     }
+    // }
+    const map = new Map();
+    
     for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) {
-            return participant[i];
-        }
+        let a = participant[i];
+        let b = completion[i];
+        
+        map.set(a, (map.get(a) || 0) + 1)
+        map.set(b, (map.get(b) || 0) - 1)
     }
+    for (let [k, v] of map) {
+        if (v > 0) return k;
+    }
+    return "none";
 }
