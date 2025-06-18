ec0fab238f62be0240ac67d1f9fed55e5ab6128c
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-06-18 23:26:14 +0900

    Update cyclic_rotation.js

--- codility/cyclic_rotation.js
+++ codility/cyclic_rotation.js
@@ -4,3 +4,19 @@ function solution(A, K) {
 
     return shifted.concat(A);
 }
+
+---
+
+function solution(A, K) {
+    let rotate = K % A.length;
+
+    if (rotate === 0) return A;
+
+    while (rotate > 0) {
+        const last = A.pop();
+        A.splice(0, 0, last)
+        rotate--;
+    }
+
+    return A;
+}
