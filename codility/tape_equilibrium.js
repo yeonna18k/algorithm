99f1bc504ac016b60cd1e5c0ec8ada5c2e495944
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-06-22 18:50:48 +0900

    Update tape_equilibrium.js

--- codility/tape_equilibrium.js
+++ codility/tape_equilibrium.js
@@ -1,3 +1,5 @@
+Try1 (48%)
+
 function solution(A) {
     const sum = A.reduce((acc, cur) => acc + cur, 0)
     let min = sum;
@@ -6,6 +8,27 @@ function solution(A) {
     for (let i = 0; i < A.length; i++) {
         firstPart += A[i];
         const diff = Math.abs(sum - (firstPart * 2))
+        if (diff < min) {
+            min = diff
+        }
+    }
+
+    return min;
+}
+
+
+Try2 (100%)
+
+function solution(A) {
+    let firstPart = A[0];
+    let secondPart = A.reduce((acc, cur) => acc + cur, 0) - A[0]
+    let min = Math.abs(firstPart - secondPart)
+
+    for (let i = 1; i < A.length - 1; i++) {
+        firstPart += A[i]
+        secondPart -= A[i]
+
+        const diff = Math.abs(firstPart - secondPart);
         if (diff < min) {
             min = diff
         }
