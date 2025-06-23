b697130e9bfc035306863ddf28daa5dade8e2b47
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-06-23 23:40:55 +0900

    Update max_counters.js

--- codility/max_counters.js
+++ codility/max_counters.js
@@ -1,3 +1,5 @@
+Try 01 (77%)
+
 function solution(N, A) {
     const arr = new Array(N + 1).fill(0);
 
@@ -11,6 +13,32 @@ function solution(N, A) {
             arr.fill(max);
         }
     } 
+
+    return arr.slice(1);
+}
+
+---
+
+Try 02 (100%)
+
+function solution(N, A) {
+    const arr = new Array(N + 1).fill(0);
+    let max = 0;
+    let baseValue = 0;
+
+    for (let i = 0; i < A.length; i++) {
+        const num = A[i];
+        if (num <= N) {
+            if (arr[num] < baseValue) arr[num] = baseValue;
+            arr[num]++;
+            if (arr[num] > max) max = arr[num];
+        } else {
+            baseValue = max
+        }
+    } 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < baseValue) arr[i] = baseValue;
+    }
 
     return arr.slice(1);
 }
