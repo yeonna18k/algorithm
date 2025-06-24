42c3d7e9a23614abb70bea72126dc72125c635a0
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-06-24 23:34:31 +0900

    Update missing_integer.js

--- codility/missing_integer.js
+++ codility/missing_integer.js
@@ -1,4 +1,6 @@
-unction solution(A) {
+Try 01 (33%)
+
+function solution(A) {
     let min = 1;
     A.sort((a, b) => a - b)
 
@@ -14,4 +16,52 @@ unction solution(A) {
         }
     }
     return min + 1;
+}
+
+---
+
+Try 02 (88%)
+
+function solution(A) {
+    let current = 1;
+    A.sort((a, b) => a - b)
+
+    if (A.at(-1) <= 0) return 1;
+
+    if (A[0] > 1) return 1;
+
+    for (let i = 0; i < A.length; i++) {
+        if (current === A[i]) {
+            continue;
+        } else if (current + 1 === A[i]) {
+            current++;
+        } else if (current + 1 < A[i]) {
+            break;
+        }
+    }
+    return current + 1;
+}
+
+---
+
+Try 03 (100%)
+
+function solution(A) {
+    let current = 0;
+    A.sort((a, b) => a - b)
+
+    if (A.at(-1) <= 0) return 1;
+
+    if (A[0] > 1) return 1;
+
+    for (let i = 0; i < A.length; i++) {
+        if (current === A[i]) {
+            continue;
+        } else if (current + 1 === A[i]) {
+            current++;
+        } else if (current + 1 < A[i]) {
+            break;
+        }
+    }
+    return current + 1;
 }
