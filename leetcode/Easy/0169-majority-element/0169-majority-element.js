1653dda6dab336724cad32ada7328589370dcd2e
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-15 23:24:22 +0900

    Time: 0 ms (100%), Space: 56.1 MB (81.16%) - LeetHub

--- leetcode/Easy/0169-majority-element/0169-majority-element.js
+++ leetcode/Easy/0169-majority-element/0169-majority-element.js
@@ -4,18 +4,18 @@
  */
 var majorityElement = function(nums) {
     if (nums.length === 1) return nums[0];
-    nums.sort((a, b) => a - b)
 
-    let count = 0;
+    let count = 1;
+    let major = nums[0]
 
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[i - 1]) {
-            count = i - count;
-        }
-        if (count > nums.length / 2) {
-            return nums[i - 1] 
-        }
+        if (count === 0) {
+            count++;
+            major = nums[i]
+        } else if (major === nums[i]) {
+            count++;
+        } else count--
     }
 
-    return nums.at(-1)
+    return major;
 };
