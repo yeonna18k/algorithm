bdb00ed034b4fd10514c82caa401a93ce33042b8
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-15 23:12:20 +0900

    Time: 9 ms (17.39%), Space: 59.8 MB (8.02%) - LeetHub

--- leetcode/Easy/0169-majority-element/0169-majority-element.js
+++ leetcode/Easy/0169-majority-element/0169-majority-element.js
@@ -3,18 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let num = nums[0]
-    let count = 1;
+    if (nums.length === 1) return nums[0];
+    nums.sort((a, b) => a - b)
+
+    let count = 0;
 
     for (let i = 1; i < nums.length; i++) {
-        if (count === 0) {
-            num = nums[i]
-            count++;
-        } else if (num === nums[i]) {
-            count++;
-        } else {
-            count--;
+        if (nums[i] !== nums[i - 1]) {
+            count = i - count;
+        }
+        if (count > nums.length / 2) {
+            return nums[i - 1] 
         }
     }
-    return num;
+
+    return nums.at(-1)
 };
