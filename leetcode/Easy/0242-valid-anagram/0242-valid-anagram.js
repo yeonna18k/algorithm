ee201c4a054264f8311af6b75f34a2fa48042d36
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-21 11:12:17 +0900

    Time: 1396 ms (5.14%), Space: 58.9 MB (35.21%) - LeetHub

--- leetcode/Easy/0242-valid-anagram/0242-valid-anagram.js
+++ leetcode/Easy/0242-valid-anagram/0242-valid-anagram.js
@@ -3,36 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isAnagram = function(s, t) {
-    // 길이가 다르면 즉시 false 반환
-    if (s.length !== t.length) {
-        return false;
-    }
+    if (s.length !== t.length) return false;
 
-    const charCount = {};
+    const tArr = t.split("")
 
-    // s 문자열의 각 문자 개수를 센다.
-    for (const char of s) {
-        charCount[char] = (charCount[char] || 0) + 1;
-    }
-
-    // t 문자열을 순회하며 문자 개수를 줄인다.
-    for (const char of t) {
-        // t의 문자가 s에 없거나, 개수가 이미 0이면 false
-        if (!charCount[char]) {
+    for (let i = 0; i < s.length; i++) {
+        if (tArr.indexOf(s[i]) > -1) {
+            const idx = tArr.indexOf(s[i])
+            tArr.splice(idx, 1)
+        } else {
             return false;
         }
-        charCount[char]--;
     }
-
     return true;
 };
-
-// 예시
-console.log(isAnagram("anagram", "nagaram")); // true
-console.log(isAnagram("rat", "car"));       // false
