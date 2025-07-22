108d1b5a6888f43ac725da9bf07a1d2a17411e88
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-22 23:03:39 +0900

    Time: 202 ms (5.31%), Space: 67.1 MB (5.13%) - LeetHub

---
+++ leetcode/Easy/0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        console.log(map)
+        const num = target - nums[i]
+        if (map.has(num)) {
+            return [map.get(num), i]
+        } else {
+            map.set(nums[i], i)
+        }
+    }
+
+};
