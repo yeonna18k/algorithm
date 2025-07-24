09fde000bd33906ca7c531ddca43c9f090f308e9
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-24 19:15:05 +0900

    Time: 14 ms (60.67%), Space: 56.9 MB (76.08%) - LeetHub

--- leetcode/Easy/0242-valid-anagram/0242-valid-anagram.js
+++ leetcode/Easy/0242-valid-anagram/0242-valid-anagram.js
@@ -6,12 +6,19 @@
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
 
-    const tArr = t.split("")
+    const tMap = new Map();
+    for (const str of t) {
+        tMap.set(str, tMap.get(str) + 1 || 1)
+    }
 
     for (let i = 0; i < s.length; i++) {
-        if (tArr.indexOf(s[i]) > -1) {
-            const idx = tArr.indexOf(s[i])
-            tArr.splice(idx, 1)
+        if (tMap.has(s[i])) {
+            const tCount = tMap.get(s[i])
+            if (tCount > 1) {
+                tMap.set(s[i], tCount - 1)
+            } else {
+                tMap.delete(s[i])
+            }
         } else {
             return false;
         }
