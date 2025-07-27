0297a621c2159aaa700bc2d7e155dc33f23c46f8
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-27 17:00:24 +0900

    Time: 0 ms (100%), Space: 53.3 MB (59.72%) - LeetHub

--- leetcode/Easy/0228-summary-ranges/0228-summary-ranges.js
+++ leetcode/Easy/0228-summary-ranges/0228-summary-ranges.js
@@ -3,32 +3,23 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
-    if (nums.length === 1) {
-        return nums.map(n => n.toString())
-    } else if (nums.length === 0) {
-        return nums;
-    }
-    
+    if (nums.length === 0) return [];
+
     const arr = [];
     let start = nums[0];
     
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i - 1] + 1) {
-            continue
-        } else {
-            if (start === nums[i - 1]) {
+    for (let i = 1; i <= nums.length; i++) {
+        if (i === nums.length || nums[i] !== nums[i - 1] + 1) {
+            const end = nums[i - 1]
+            if (start === end) {
                 arr.push(`${start}`)
             } else {
-                arr.push(`${start}->${nums[i - 1]}`)
+                arr.push(`${start}->${end}`)
             }
-            start = nums[i];
+            if (i < nums.length) {
+                start = nums[i]
+            }
         }
-    }
-
-    if (start === nums.at(-1)) {
-        arr.push(`${start}`)
-    } else {
-        arr.push(`${start}->${nums.at(-1)}`)
     }
     return arr;
 };
