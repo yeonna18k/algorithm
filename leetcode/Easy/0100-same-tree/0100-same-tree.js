8a04fd93bf44e3a86e8b4eb41c465a98bee0bd25
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-07-29 23:57:00 +0900

    Time: 1 ms (4.59%), Space: 53.2 MB (81.1%) - LeetHub

---
+++ leetcode/Easy/0100-same-tree/0100-same-tree.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+const isSameTree = function(p, q) {
+    if (!p && !q) {
+        return true;
+    }
+
+    if (!p || !q || p.val !== q.val) {
+        return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
