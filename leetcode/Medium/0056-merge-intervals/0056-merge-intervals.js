a561412e2aa9a041116c793e9631be5e1f6f5008
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-08-08 22:04:42 +0900

    Time: 8 ms (71.6%), Space: 64.2 MB (77.95%) - LeetHub

---
+++ leetcode/Medium/0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const merged = [intervals[0]];
+for (let i = 1; i < intervals.length; i++) {
+        const currentInterval = intervals[i];
+        const lastMerged = merged[merged.length - 1];
+
+        if (lastMerged[1] >= currentInterval[0]) {
+            lastMerged[1] = Math.max(lastMerged[1], currentInterval[1]);
+        } else {
+            merged.push(currentInterval);
+        }
+    }
+
+    return merged;
+};
