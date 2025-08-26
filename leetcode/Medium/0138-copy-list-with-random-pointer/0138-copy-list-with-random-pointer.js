d45b41e5e98b0255fcd0d9fa56e6ff1dae05f1f2
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-08-26 17:47:35 +0900

    Time: 50 ms (20.85%), Space: 56.8 MB (9.38%) - LeetHub

---
+++ leetcode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {_Node} head
+ * @return {_Node}
+ */
+var copyRandomList = function(head) {
+    if (!head) return null;
+
+    const map = new Map();
+
+    let current = head;
+    while (current) {
+        map.set(current, new _Node(current.val));
+        current = current.next;
+    }
+
+    current = head;
+    while (current) {
+        const copiedNode = map.get(current);
+        copiedNode.next = map.get(current.next) || null;
+        copiedNode.random = map.get(current.random) || null;
+        current = current.next;
+    }
+
+    return map.get(head);
+};
