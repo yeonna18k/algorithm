e539a469c2b3e5f99d4e9cf9c2b4fbbdc79e4184
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-09-09 12:42:46 +0900

    Time: 73 ms (4.81%), Space: 58.6 MB (99.17%) - LeetHub

--- leetcode/Easy/0344-reverse-string/0344-reverse-string.js
+++ leetcode/Easy/0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,8 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse()
+    for (let i = 0; i < s.length / 2; i++) {
+        [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]];
+    }
+    return s;
 };
