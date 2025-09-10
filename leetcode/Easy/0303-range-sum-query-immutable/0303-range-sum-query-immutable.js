c1a711393fe99a98170ad52c04fb77335c08b234
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-09-10 16:55:34 +0900

    Time: 80 ms (16.51%), Space: 61.6 MB (91.31%) - LeetHub

---
+++ leetcode/Easy/0303-range-sum-query-immutable/0303-range-sum-query-immutable.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    let i = 0;
+    let sum = 0;
+
+    while (left + i <= right) {
+        sum += this.nums[left + i]
+        i++;
+    }
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
