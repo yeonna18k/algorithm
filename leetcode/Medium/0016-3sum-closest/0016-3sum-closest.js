ce3c553b8516a302c74601123187f3562ec73618
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-09-11 23:50:47 +0900

    Time: 14 ms (68.27%), Space: 57.5 MB (8.29%) - LeetHub

---
+++ leetcode/Medium/0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+        nums.sort((a, b) => a - b);
+    
+    let closestSum = nums[0] + nums[1] + nums[2];
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+
+            if (sum < target) {
+                left++; 
+            } else if (sum > target) {
+                right--; 
+            } else {
+                return closestSum;
+            }
+        }
+    }
+
+    return closestSum;
+};
