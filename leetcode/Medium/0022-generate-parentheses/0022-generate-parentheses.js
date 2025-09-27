0d92108be927b3dcf952065b39f15d0025d4cba3
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-09-27 23:53:09 +0900

    Time: 0 ms (100%), Space: 56.6 MB (12.11%) - LeetHub

---
+++ leetcode/Medium/0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = [];
+
+    function generate(current, open, close) {
+        if (current.length === 2 * n) {
+            result.push(current);
+            return;
+        }
+
+        if (open < n) {
+            generate(current + '(', open + 1, close);
+        }
+
+        if (close < open) {
+            generate(current + ')', open, close + 1);
+        }
+    }
+
+    generate("", 0, 0);
+
+    return result;
+};
