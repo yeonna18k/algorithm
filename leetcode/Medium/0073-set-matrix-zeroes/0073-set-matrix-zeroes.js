d4a05d6fef68d7f525307554493d57f531ddff14
nayeon <134368869+yeonna18k@users.noreply.github.com>
2025-10-15 22:49:03 +0900

    Time: 0 ms (100%), Space: 60.1 MB (18.34%) - LeetHub

---
+++ leetcode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const n = matrix.length;
+    const m = matrix[0].length;
+
+    let col0 = 1;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                if (j !== 0) {
+                    matrix[0][j] = 0;
+                } else {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < m; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (matrix[0][0] === 0) {
+        for (let j = 0; j < m; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (col0 === 0) {
+        for (let i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+};
